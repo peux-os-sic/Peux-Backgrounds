6203fe575d25c71c22074f23fda13e6d43a38a40
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-29 15:46:13 +0530

    commit

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-PASSWD="$(zenity --password --title=Authentication)\n"
+if zenity --question --text="Want to copy the configurations?"
+then 
+    PASSWD="$(zenity --password --title=Authentication)\n"
 
-echo "changing directory"
-cd ../
-echo -e $PASSWD | sudo -S cp -r Peux-Backgrounds/backgrounds/* /usr/share/backgrounds
-rm -rf Peux-Backgrounds
+    echo "changing directory"
+    cd ../
+    echo -e $PASSWD | sudo -S cp -r Peux-Backgrounds/backgrounds/* /usr/share/backgrounds
+    rm -rf Peux-Backgrounds
+    notify-send "Copied the images"
 
-
+else
+    notify-send "Operation skipped!"
+fi
