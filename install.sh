8c792264a95e14e4601e7ea4d5eb765e1f5e519f
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-29 12:08:38 +0530

    commit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PASSWD="$(zenity --password --title=Authentication)\n"
 
 echo "changing directory"
 cd ../
-echo -e $PASSWD | sudo -S cp -r Peux-Backgrounds/backgrounds/ /usr/share/backgrounds
+echo -e $PASSWD | sudo -S cp -r Peux-Backgrounds/backgrounds/* /usr/share/backgrounds
 rm -rf Peux-Backgrounds
 
 
